6d6efad7af8bfe1bc2bf991181be82c90851bf06
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-05-03 10:06:07 +0300

    .

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,16 @@ main :: IO ()
 main = mainHorse
 
 mainHorse = do 
-   putStrLn " nxy> "
+   putStrLn " r c x y > "
    s <- getLine
-   let [n, x ,y] = [read [x] | x <- s, x /= ' ']
+   let [r, c, x ,y] = [read [x] | x <- s, x /= ' ']
 
    t0 <- getCurrentTime
    print $ "--- start --- " ++ show t0
    ---
-   print $ runHorse n (x, y)
+   let path = head $ runHorse r c (x, y)
+   print $ length path
+   putStrLn $ showPath path r c
    ---
    t1 <- getCurrentTime
    putStrLn $ "--- time --- " ++ show (diffUTCTime t1 t0)
--- src/Horse.hs
+++ src/Horse.hs
@@ -1,29 +1,48 @@
-module Horse (runHorse) where
-
+module Horse (runHorse, showPath) where
 -- Обійти конем усі клітинки шахівниці розміром n х n, 
 -- побувавши рівно один раз на кожній клітинці.
+
+import Consul
+import Data.List ( sort )
+
 type Pos = (Int, Int)
 type Path  = [Pos]
 
-runHorse :: Int -> Pos -> Path
-runHorse n pos = head $ doStep [[pos]] 1 n
 
-doStep :: [Path] -> Int -> Int -> [Path]
-doStep paths len n | len == n * n = paths
-doStep paths len n = do
+runHorse :: Int -> Int -> Pos -> [Path]
+runHorse nR nC pos = doStep [[pos]] 1 nR nC
+
+doStep :: [Path] -> Int -> Int -> Int ->  [Path]
+doStep paths len nR nC | len == nR * nC = paths
+doStep paths len nR nC = do
    path <- paths
    let lastPos = head path
-   let steps = getSteps lastPos n path
+   let steps = getSteps lastPos nR nC path
    step <- steps
-   doStep [step : path] (len + 1) n
+   doStep [step : path] (len + 1) nR nC
 
 
-getSteps :: Pos -> Int -> Path -> [Pos]
-getSteps (x, y) n path = filter f [
-   -- (x-1, y-2), (x+1, y+2), (x+2, y-1), (x-2, y+1), 
-   -- (x-2, y-1), (x+2, y+1), (x+1, y-2), (x-1, y+2) ]     
-    (x+1, y+2), (x+1, y-2), (x-1, y+2), (x-1, y-2),
-    (x+2, y+1), (x+2, y-1), (x-2, y+1), (x-2, y-1)]
+getSteps :: Pos -> Int -> Int -> Path -> [Pos]
+getSteps (r, c) nR nC path = filter f [
+    (r+1, c+2), (r+1, c-2), (r-1, c+2), (r-1, c-2),
+    (r+2, c+1), (r+2, c-1), (r-2, c+1), (r-2, c-1)]
  where
-   f (x, y) = x >= 0 && y >= 0 && x < n && y < n && notElem (x, y) path
+   f (r, c) = r >= 0 && c >= 0 && r < nR && c < nC && notElem (r, c) path
+
+showPath :: Path -> Int -> Int -> String
+showPath path nRows nCols = let
+   path' :: [String] 
+   path' = map (show2 . snd) (sort $ zip (reverse path) [1..])
+   show2 n =  (if n < 10 then " " ++ show n else show n) ++ " "
+   in
+     concat $ insN path' "\n" nCols
+    
+
+{-- 
+Є рядок s, символ r і число n. Треба вставити символ r в рядок s через однакові проміжки у n символів.
+--}
+
+insN ::  [a] -> a -> Int -> [a]
+insN cs a n = concat [ if i `mod` n == (n-1) then [nC, a] else [nC] | (i, nC) <- zip [0..] cs] 
+
 
