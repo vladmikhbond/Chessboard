e85a26dce8aa7307f12ca579ff547ed446bd2cba
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-05-05 08:51:06 +0300

    old style

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,46 @@ module Main where
 import Queens
 import Horse
 import Data.Time.Clock ( diffUTCTime, getCurrentTime )
+import System.IO (hFlush, stdout )
+import Data.List.Split ( splitOn )
 
 main :: IO ()
 main = mainHorse
 
-mainHorse = do 
-   putStrLn " r c x y > "
-   s <- getLine
-   let [r, c, x ,y] = [read [x] | x <- s, x /= ' ']
-
+mainHorse = do
+   (r, c) : start : finish <- getPairs " rc st fi > "
+   --closed <- getPairs " closed: xy xy ... > "
+   let closed = []
+   ---------------------------------------
    t0 <- getCurrentTime
    print $ "--- start --- " ++ show t0
    ---
-   let path = head $ runHorse r c (x, y)
-   print $ length path
-   putStrLn $ showPath path r c
+   let pathes = runHorse r c start closed
+   let validPathes = if null finish 
+       then pathes 
+       else filter (\p -> head p == head finish ) pathes 
+   if null validPathes
+   then putStr "no answers"
+   else do
+      let path = head validPathes
+      putStrLn $ showOnePath path r c closed
    ---
    t1 <- getCurrentTime
    putStrLn $ "--- time --- " ++ show (diffUTCTime t1 t0)
 
 
+getPairs :: String -> IO [(Int, Int)]
+getPairs prompt = do
+   putStr prompt
+   hFlush stdout
+   str <- getLine
+   let ns = if null str 
+       then [] 
+       else read <$> splitOn " " str
+   return $ map (`divMod` 10) ns
+   
+
+
+
+
+  
--- src/Horse.hs
+++ src/Horse.hs
@@ -1,48 +1,83 @@
-module Horse (runHorse, showPath) where
+module Horse (runHorse, showOnePath) where
 -- Обійти конем усі клітинки шахівниці розміром n х n, 
 -- побувавши рівно один раз на кожній клітинці.
 
 import Consul
 import Data.List ( sort )
+import Data.List.Split ( chunksOf )
 
 type Pos = (Int, Int)
 type Path  = [Pos]
 
+doStep :: [Path] -> Int -> Int -> Int -> [Pos] -> [Path]
+doStep paths len nR nC closed = doStepR paths len
+ where
+   doStepR paths len
+      | len == nR * nC - length closed = paths
+   doStepR paths len = do
+      path <- paths
+      let lastPos = head path
+      let steps = getStepsOrd lastPos nR nC path closed
+      step <- steps
+      doStepR [step : path] (len + 1)
 
-runHorse :: Int -> Int -> Pos -> [Path]
-runHorse nR nC pos = doStep [[pos]] 1 nR nC
 
-doStep :: [Path] -> Int -> Int -> Int ->  [Path]
-doStep paths len nR nC | len == nR * nC = paths
-doStep paths len nR nC = do
-   path <- paths
-   let lastPos = head path
-   let steps = getSteps lastPos nR nC path
-   step <- steps
-   doStep [step : path] (len + 1) nR nC
-
-
-getSteps :: Pos -> Int -> Int -> Path -> [Pos]
-getSteps (r, c) nR nC path = filter f [
+getSteps :: Pos -> Int -> Int -> Path -> [Pos] -> [Pos]
+getSteps (r, c) nR nC path closed = filter f [
     (r+1, c+2), (r+1, c-2), (r-1, c+2), (r-1, c-2),
     (r+2, c+1), (r+2, c-1), (r-2, c+1), (r-2, c-1)]
  where
-   f (r, c) = r >= 0 && c >= 0 && r < nR && c < nC && notElem (r, c) path
+   f (r, c) = r >= 0 && c >= 0 && r < nR && c < nC
+              && notElem (r, c) path
+              && notElem (r, c) closed
 
-showPath :: Path -> Int -> Int -> String
-showPath path nRows nCols = let
-   path' :: [String] 
-   path' = map (show2 . snd) (sort $ zip (reverse path) [1..])
-   show2 n =  (if n < 10 then " " ++ show n else show n) ++ " "
-   in
-     concat $ insN path' "\n" nCols
-    
+getStepsOrd :: Pos -> Int -> Int -> Path -> [Pos] -> [Pos]
+getStepsOrd (r, c) nR nC path closed = let
+   gs = getSteps (r, c) nR nC path closed
+   pricedSteps :: [(Int, Pos)]
+   pricedSteps = map (\s -> (length $ getSteps s nR nC (s : path) closed, s)) gs 
+ in
+    map snd (sort pricedSteps)  
+
+
+
+-- UTILS ------------------------------------------------
 
 {-- 
 Є рядок s, символ r і число n. Треба вставити символ r в рядок s через однакові проміжки у n символів.
+
+insSepN xs sep n = undefined
 --}
 
-insN ::  [a] -> a -> Int -> [a]
-insN cs a n = concat [ if i `mod` n == (n-1) then [nC, a] else [nC] | (i, nC) <- zip [0..] cs] 
+insN ::  Int -> [a] -> a -> [a]
+insN n xs sep = let
+   chunks = chunksOf n xs
+ in
+   head chunks ++ concatMap (sep :) (tail chunks)
+
+-- SHOW -------------------------------------------------
+
+showOnePath :: Path -> Int -> Int -> [Pos] -> String
+showOnePath path nRows nCols closed =
+   concat $ insN nCols path2 "\n"
+ where
+   pathNclosed = reverse path ++ closed
+   pairs :: [(Pos, Int)]
+   pairs = sort $ zip pathNclosed ([1..length path] ++ repeat 0 )   -- 
+   path2 = map (show2 . snd) pairs  -- sort on positions
+
+   show2 0 = "   "
+   show2 n | n < 10 = "  " ++ show n
+   show2 n = " " ++ show n
+
+runHorse :: Int -- n rows
+ -> Int         -- n cols
+ -> Pos         -- start
+ -> [Pos]       -- closed cells
+ -> [Path]
+runHorse nRows nCols pos = doStep [[pos]] 1 nRows nCols
+
+
+
 
 
